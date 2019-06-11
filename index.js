f7246ba307ea8ce9763b948f5cd10a46de7603e7
jenya purik <jenyaglor@gmail.com>
2019-06-11 23:48:57 +0300

    first comit validating email

---
+++ index.js
@@ -0,0 +1,55 @@
+ let inpData = document.getElementById('input')
+        let btn = document.getElementById('btn')
+        let small = document.getElementById('small')
+        btn.addEventListener('click',()=>{
+
+            if(inpData.value === ''){
+              document.getElementById('small').innerHTML = 'Email cannot be empty'
+              document.getElementById('small').style.color = "red";
+            }else{
+                document.getElementById('small').innerHTML = 'mailboxlayer.com API'
+                document.getElementById('small').style.color = "grey";
+                console.log(inpData.value)
+                let access_key = '333d64ccc8ad0d0051adf39d6ed460fc';
+                let email_address = inpData.value;
+
+                fetch('http://apilayer.net/api/check?access_key=' + access_key + '&email=' + email_address, {method: 'POST'}
+                    ).then(function(response) {
+                let contentType = response.headers.get("content-type");
+                if(contentType && contentType.includes("application/json")) {
+                  return response.json();
+                }
+                throw new TypeError("Oops, we haven't got JSON!");
+              })
+              .then(function(json) {  
+
+                if(json.format_valid === false){
+                   document.getElementById('small').innerHTML = 'Email not valid'
+                   document.getElementById('small').style.color = "red";
+                }else if(json.format_valid === true){
+                   document.getElementById('small').innerHTML = 'Email valid'
+                   document.getElementById('small').style.color = "green";
+                }
+                console.log(json.format_valid);
+                console.log(json.smtp_check);
+                console.log(json.score);
+                console.log(json);
+
+             }).catch(function(error) { console.log(error); });
+
+
+              $(document).ready(function() {
+                let $click = $('.clickhere');
+                $click.mbClicker({
+                        size: 300, //Maximum size of circle
+                        speed: 750, //Time of animation in miliseconds
+                        colour: 'rgba(0,0,0,0.11)', //Colour of circle and shadow
+                        shadow: false, //Whether or not to have a shadow on the circle
+                        buttonAnimation: true //Only use if button doesn't have a style attribute
+                });
+            });
+
+
+            }
+             
+})
